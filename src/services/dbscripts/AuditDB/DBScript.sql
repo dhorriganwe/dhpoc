35f74fb1d2019ad15e5c9415f98275fd15ae233d
dhorriganwe <dhorrigan@wilburellis.com>
2014-12-05 19:02:43 -0800

    update dbscripts

--- src/services/dbscripts/AuditDB/DBScript.sql
+++ src/services/dbscripts/AuditDB/DBScript.sql
@@ -1,13 +1,11 @@
 \encoding UTF8
-\connect rtaudit_20141204
+\connect rtaudit_20141124
 \encoding UTF8
 
 \ir SchemaChanges.sql
 
 \ir ./rt.AddAuditLog.sql
 \ir ./rt.GetApplicationNames.sql
-\ir ./rt.GetCategories.sql
-\ir ./rt.GetFeatureNames.sql
 \ir ./rt.GetAuditLogAll.sql
 \ir ./rt.GetAuditLogByApplicationName.sql
 \ir ./rt.GetAuditLogByCategory.sql
@@ -16,3 +14,9 @@
 \ir ./rt.GetAuditLogById.sql
 \ir ./rt.GetAuditLogByTraceLevel.sql
 \ir ./rt.GetAuditLogSummary.sql
+\ir ./rt.GetCategories.sql
+\ir ./rt.GetFeatureNames.sql
+\ir ./rt.GetSummaryByApplicationName.sql
+\ir ./rt.GetSummaryByCategory.sql
+\ir ./rt.GetSummaryByFeatureName.sql
+
