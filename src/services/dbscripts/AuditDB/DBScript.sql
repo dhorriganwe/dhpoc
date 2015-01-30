e0e049670bbe6a75b86396480aad41bbd96f236d
dhorriganwe <dhorrigan@wilburellis.com>
2015-01-30 15:27:38 -0800

    normalize MaxRowCount

--- src/services/dbscripts/AuditDB/DBScript.sql
+++ src/services/dbscripts/AuditDB/DBScript.sql
@@ -1,23 +1,32 @@
 \encoding UTF8
-\connect Audit
+\connect rtaudit_20141124
 \encoding UTF8
 
 \ir SchemaChanges.sql
 
 \ir ./rt.AddAuditLog.sql
+\ir ./rt.GetApplicationNameCounts.sql
 \ir ./rt.GetApplicationNames.sql
 \ir ./rt.GetAuditLogAll.sql
+
 \ir ./rt.GetAuditLogByApplicationName.sql
 \ir ./rt.GetAuditLogByCategory.sql
-
 \ir ./rt.GetAuditLogByEventId.sql
 \ir ./rt.GetAuditLogByFeatureName.sql
 \ir ./rt.GetAuditLogByFilters.sql
+
 \ir ./rt.GetAuditLogById.sql
 \ir ./rt.GetAuditLogByTraceLevel.sql
 \ir ./rt.GetAuditLogRowCount.sql
 
+\ir ./rt.GetByILikeAdditionalInfo.sql
+\ir ./rt.GetByILikeEventId.sql
+\ir ./rt.GetByILikeLoginName.sql
+\ir ./rt.GetByILikeMessage.sql
+
 \ir ./rt.GetCategories.sql
+\ir ./rt.GetCategoryCounts.sql
+\ir ./rt.GetFeatureNameCounts.sql
 \ir ./rt.GetFeatureNames.sql
 \ir ./rt.GetSummaryByApplicationName.sql
 \ir ./rt.GetSummaryByCategory.sql
